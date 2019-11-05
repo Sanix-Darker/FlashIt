b971962549e44dd2a236b4e05e77701b1544a5cd
sanix-darker <s4nixd@gmail.com>
2019-11-05 20:05:45 +0100

    Cleaning stuff on api/main.py

--- api/main.py
+++ api/main.py
@@ -45,41 +45,33 @@ def index1():
 @app.route('/flash', methods=['GET']) # To prevent Cors issues
 def index2():
     # Let's protect the main api from crashing
-    #try:
+    try:
         print("Fetching results...")
         # Sent in GET requests
-        host = request.args.get('host')    
+        host = request.args.get('host')
         find = str(request.args.get('find')).replace(" ", "+")
         percent = request.args.get('percent')
-        try:
-            level = int(request.args.get('level'))
-        except:
-            level = 5
-            
-        try:
-            category = request.args.get('category')
-        except:
-            category = "0"
-            
+        try: level = int(request.args.get('level'))
+        except Exception as es: level = 5
+
+        try: category = request.args.get('category')
+        except Exception as es: category = "0"
+
         price = request.args.get('price')
-        
-        try:
-            job = request.args.get('job')
-        except:
-            job = None
-        
+
+        try: job = request.args.get('job')
+        except Exception as es: job = None
+
         code = ""
         results_size = 0
         json_results = []
         percent_classes = ["sale-flag-percent"]
         price_classes = ["price"]
         for i in range(1, level):
-            search = ""
-            if category != "0":
-                search = category.replace("https://www.jumia.cm", host+"/")+"&page="+str(i)
-            else:
-                search = host+"/catalog/?q="+find+"&page="+str(i)
-            
+
+            search = host+"/catalog/?q="+find+"&page="+str(i)
+            if category != "0": search = category.replace("https://www.jumia.cm", host+"/")+"&page="+str(i)
+
             print("search: ", search)
             results = getElts(requests.get(search))
             results_size += len(results)
@@ -91,11 +83,11 @@ def index2():
                         print("- Href: "+result["href"])
                         # print("price: ", price)
                         # print("percent: ", percent)
-                        
+
                         if (price == "0" and percent != "0" and child["class"][0] in percent_classes and float(percent) <= float(only_numbers(child.text))):
                             #print("- Href: "+result["href"],">>>> ", child.text)
                             json_results.append({"title": str(result.text), "href": str(result["href"]), "percent": float(only_numbers(child.text)), "price": 0})
-                                
+
                         elif(price != "0" and percent == "0" and child["class"][0] == "price" and "-old" not in child["class"]):
                             the_pricechildren = child.findChildren("span" , recursive=True)
                             product_price = int(the_pricechildren[0]["data-price"])
@@ -106,15 +98,13 @@ def index2():
                             if (child["class"][0] in price_classes and float(price.split("-")[1]) > float(product_price)):
                                 #print("- Href: "+result["href"],">>>> ", child.text)
                                 json_results.append({"title": str(result.text), "href": str(result["href"]), "percent": 0, "price": product_price})
-                    except:
+                    except Exception as es:
                         pass
             i = i + 1
 
-        if price == "0":
-            json_results = sorted(json_results, key=lambda k: k['percent'])[::-1]
-        else:
-            json_results = sorted(json_results, key=lambda k: k['price'])[::-1]
-            
+        json_results = sorted(json_results, key=lambda k: k['price'])[::-1]
+        if price == "0": json_results = sorted(json_results, key=lambda k: k['percent'])[::-1]
+
         if (len(json_results) == 0 and job == None):
             code = str(MD5(search))[:10]
             _date = (str(datetime.now())).split('.')[0]
@@ -126,13 +116,13 @@ def index2():
             c.close()
         # Build the response
         response = jsonify({'status':'success', 'code':code,  'fetched': str(results_size)+' fetched', 'filtered': str(len(json_results))+' filtered', "results": json_results })
-    # except:
-    #     print("Oups, some error occurs!")
-    #     response = jsonify({'status':'error', 'message':"Your request cause an unexcepted error on the server"})
-        
+    except Exception as es:
+        print("Oups, some error occurs!", str(es))
+        response = jsonify({'status':'error', 'message':"Your request cause an unexcepted error on the server"})
+
     # Let's allow all Origin requests
-        response.headers.add('Access-Control-Allow-Origin', '*') # To prevent Cors issues
-        return response
+    response.headers.add('Access-Control-Allow-Origin', '*') # To prevent Cors issues
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=7777)
